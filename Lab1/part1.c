09da35c3749b03b99debf75805d9e4e94b8be9fa
WolffM <matthauswolff@gmail.com>
2016-02-12 15:05:31 -0500

    part 1 complete

--- Lab1/part1.c
+++ Lab1/part1.c
@@ -5,3 +5,30 @@
 
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int p1;
+	p1 = fork();
+
+	if(p1 == 0){
+		int p2;
+		p2 = fork();
+
+		if(p2 == 0){
+			printf("\ngrandchild has pid of: %d \n", getpid());
+			sleep(1);
+		}
+		else if (p2 != 0){
+			printf("\nchild has pid of: %d \n", getpid());
+			waitpid(-1,NULL,0);
+			
+		}
+	}
+	else if(p1 != 0){
+		printf("\nparent has pid of: %d \n", getpid());
+		waitpid(-1,NULL,0);
+	}
+	return 0;
+}
