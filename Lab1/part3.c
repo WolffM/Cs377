ca8d7d1267d2947a1ac1a73d5d28615fccd3d516
Steven Lambrou <slambrou@umass.edu>
2016-02-15 21:19:34 -0500

    fixed formatting so its more readable

--- Lab1/part3.c
+++ Lab1/part3.c
@@ -19,38 +19,46 @@ int main(){
 		printf("batch-shell> ");
 		fgets(buffer,1000,stdin);
 		buffer[strlen(buffer)-1] = 0;
-		if(strcmp(buffer,"q") == 0){
-			printf("EXITING BATCH-SHELL\n");
-			exit(1);
-		}
+
+			if(strcmp(buffer,"q") == 0){
+				printf("EXITING BATCH-SHELL\n");
+				exit(1);
+			}
 			
-	char* reduce;
-	int finish;
-	char* clone[1000];
-    int pid;
-	reduce = strtok(buffer," ");
-	char* tmp;
-	strcpy(tmp,"./");
-	
-	strcat(tmp,reduce);
-    while (reduce != NULL) {
-       pid = fork();
-	if(pid==0){
-			printf("MY PROCESS ID = %d\n",getpid());
-	        int finish = system(tmp);
-	        exit(1);
-		}
-		else{int status = 0;
-		    
-	        reduce = strtok(NULL," ");
-	        if(reduce){
-	        memset(tmp,0,sizeof(tmp));
-	        strcpy(tmp,"./");
-	        strcat(tmp,reduce);
-	        }
-		    wait(&status);}
-		
-    }
+			char* reduce;
+			int finish;
+			char* clone[1000];
+		    int pid;
+			reduce = strtok(buffer," ");
+			char* tmp;
+			strcpy(tmp,"./");
+			strcat(tmp,reduce);
+
+    			while (reduce != NULL) {
+			       pid = fork();
+
+				if(pid==0){
+						printf("MY PROCESS ID = %d\n",getpid());
+				        int finish = system(tmp);
+				        exit(1);
+					}
+					
+					else{
+
+						int status = 0;
+				        reduce = strtok(NULL," ");
+
+				        	if(reduce){
+
+						        memset(tmp,0,sizeof(tmp));
+						        strcpy(tmp,"./");
+						        strcat(tmp,reduce);
+
+				       		}
+
+					    wait(&status);}
+					
+			    }
 
   }
 	return 0;
