1e952b41c5f7f446d24dc8fd610a64582c5a4425
WolffM <matthauswolff@gmail.com>
2016-02-15 19:31:57 -0500

    working on 3

--- Lab1/part3.c
+++ Lab1/part3.c
@@ -7,17 +7,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main(){
 
-	char buffer[1000];
+	char frontbuff[1000];
+	char final[1000];
+
 
 	while (1) {
 		printf("batch-shell> ");
 		fgets(buffer,1000,stdin);
 
+	char* reduce;
+	int finish;
+	char* clone[1000];
 
+	reduce = strtok(buffer,tmp);
+    while (reduce!=NULL) {
+		temp[c] = reduce;
+		c++;
+    }
+	for(int x=0; x < c; x++){
+		if(fork()==0){
+	        strcat(final,clone[x]);
+	        strtok(final,"\n");
 
-	}
+	        finish = execl(bin,buffer,NULL);
+		}
+		waitpid(-1,NULL,0);
+    }
+  }
 	return 0;
 }
