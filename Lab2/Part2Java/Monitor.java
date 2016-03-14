13671bd255884168d41bf2266849d48e4af392f0
WolffM <mjwolff@umass.edu>
2016-03-13 20:35:41 -0400

    Update Monitor.java

--- Lab2/Part2Java/Monitor.java
+++ Lab2/Part2Java/Monitor.java
@@ -1,27 +1,28 @@
 Class Monitor{
-    Public Monitor{
-      int count = 0;
-      int last = 0;
-      item[] buffer;
-      int size = buffer.size();
+        public Monitor(int s)
+        {
+            int counter = 0;
+            int size = s;
+        }
 
-public void Append(item){
-  lock.Acquire();
-  if(count == buffer.N){
-    empty.Wait(lock);
-  }
-  buffer[last] = item;
-  last = (last+1)%
-  count++;
-  full.Signal();
-  lock.Release();
-}
-public void Remove(item){
-  lock.Acquire();
-  if(count == 0)
-    full.Wait(lock);
-  item = buffer[(last-count)%N];
-  count--;
-  empty.Signal();
-  lock.Release();
-}
+        synchronized void insert()
+        {
+            while(counter == size){
+                wait();
+            }
+            counter++;
+            System.out.println("Producer: " + counter);
+            if(counter == 1)
+                notify();
+        }
+
+        synchronized void remove()
+        {
+            while(counter == 0){
+                wait();
+            }
+            counter--;
+            System.out.println("Consumer: " + counter);
+            if(counter == (size-1))
+                notify();
+        }
