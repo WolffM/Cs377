5260d1e8828d484069ab017d202aa55bb7bbbe18
WolffM <mjwolff@umass.edu>
2016-03-13 22:26:53 -0400

    Update Producer.java

--- Lab2/Part2Java/Producer.java
+++ Lab2/Part2Java/Producer.java
@@ -3,15 +3,22 @@
 
 public class Producer extends Thread{
     Monitor locker;
+    int s, l;
     
-    public Producer(Monitor m){
+    public Producer(Monitor m, int sleeptime, int maxlength){
     locker = m;
+    s = sleeptime;
+    l = maxlength;
     }
-        @Override
-        public void run() {
-            while(true)
-            {
-              locker.enqueue();
-            }
+    public void run() {
+        while(1)
+        {
+        int reqLength = Math.floor(Math.random()*l);
+        int ID = Math.floor(Math.random()*100);
+          int[] request = (ID, reqLength);
+          locker.enqueue(request);
+          System.out.println("Producer: sleeping for "+s+" seconds");
+          sleep(s);
         }
     }
+}
