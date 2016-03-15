8611b5f4cf689c14ff7b38e8a65296545d5d6be3
WolffM <mjwolff@umass.edu>
2016-03-15 18:44:03 -0400

    Update Monitor.java

--- Lab2/Part2Java/Monitor.java
+++ Lab2/Part2Java/Monitor.java
@@ -1,9 +1,10 @@
 #import <stdio.h>;
-#import java.util.concurrent.*;
+#import java.util.*;
 
 Class Monitor{
         public Monitor(int s)
         {
+            Queue<int[]> MasterQueue = new LinkedList<int[]>();
             int counter = 0;
             int size = s;
         }
@@ -13,19 +14,22 @@ Class Monitor{
             while(counter == size){
                 wait();
             }
+            MasterQueue.add(request);
             counter++;
             System.out.println("Producer: produced request "+request[0]+", length "request[1]" seconds at time "+LocalDateTime.now());
             if(counter == 1)
                 notify();
         }
 
-        synchronized void dequeue(int[] request) throws InterruptedException
+        synchronized void dequeue() throws InterruptedException
         {
+            System.out.println("Consumer: assigned request ID "+MasterQueue.peek()[0]+", processing request for the next "+MasterQueue.peek()[0]" seconds, current time is "+LocalDateTime.now());
             while(counter == 0){
                 wait();
             }
+            MasterQueue.remove();
             counter--;
-            System.out.println("Consumer: completed request ID"+request[0]+" at time "+LocalDateTime.now());
+            System.out.println("Consumer: completed request ID"+MasterQueue.peek()[0]+" at time "+LocalDateTime.now());
             if(counter == (size-1))
                 notify();
         }
