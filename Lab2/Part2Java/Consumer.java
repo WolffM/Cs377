03d064fb145290f1db4aa4f70accbbd912b900e9
WolffM <mjwolff@umass.edu>
2016-03-15 18:43:52 -0400

    Update Consumer.java

--- Lab2/Part2Java/Consumer.java
+++ Lab2/Part2Java/Consumer.java
@@ -2,17 +2,14 @@
 #include java.lang.*
 
 public class Consumer extends Thread{
-    Monitor locker;
     
-    public Consumer(Monitor m){
-    locker = m;
+    public Consumer(){
     }
         @Override
         public void run() {
             while(1)
             {
-              System.out.println("Consumer i: assigned request ID "+request[0]+", processing request for the next "+request[1]+" seconds, current time is "+LocalDateTime.now());
-              locker.dequeue();
+              Queue.locker.dequeue();
             }
         }
     }
