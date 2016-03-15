eff69b052ae873d060718ee372871dcafc8fc9ab
WolffM <mjwolff@umass.edu>
2016-03-15 19:33:41 -0400

    Fin Main

--- Lab2/Part2Java/Main.java
+++ Lab2/Part2Java/Main.java
@@ -1,21 +1,19 @@
-class Main{
-	public Monitor locker;
-}
-
+public class Main{
+	public static Monitor locker;
 /*
 Args should come in the order:
-
 0 = number of consumers
 1 = max duration of a request
 2 = sleep time for producer
 */
-public static void main(int[] args) {
-	locker = new Monitor(10);
-	
-	Producer prod1 = new Producer(args[2], args[1]);
-	prod1.start();
-	
-	for(int x = args[0]; x>0; x--){
-	   new Consumer().start();
-	}
+  public static void main(String[] args) {
+    locker = new Monitor(10);
+
+    Producer prod1 = new Producer(Integer.parseInt(args[2]), Integer.parseInt(args[1]));
+    prod1.start();
+
+    for(int x = Integer.parseInt(args[0]); x>0; x--){
+      new Consumer().start();
+    }
+  }
 }
