7ca6cd98d434a4a728ac8863e7a7a7e6b66b9cd3
WolffM <mjwolff@umass.edu>
2016-03-16 15:52:16 -0400

    added sleep

--- Lab2/Part2Java/Consumer.java
+++ Lab2/Part2Java/Consumer.java
@@ -8,7 +8,10 @@ public class Consumer extends Thread{
             while(true)
             {
               try{
+                int[] temp = Main.MasterQueue.peek();
                 Main.locker.dequeue();
+                sleep(temp[1]);
+                System.out.println("Consumer: completed request ID"+temp[0]+" at time "+System.currentTimeMillis());
               }
               catch(InterruptedException e){
               	System.out.println("Exception at Consumer dequeue");
