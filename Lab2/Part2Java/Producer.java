47973078f81b2d56fad441801067c510af3fc22c
WolffM <mjwolff@umass.edu>
2016-03-16 14:14:16 -0400

    Comments

--- Lab2/Part2Java/Producer.java
+++ Lab2/Part2Java/Producer.java
@@ -3,6 +3,7 @@ import java.lang.*;
 public class Producer extends Thread{
     int s, l, ID, reqLength;
     
+    //Parameters taken from args[] in Main
     public Producer(int sleeptime, int maxlength){
       s = sleeptime;
       l = maxlength;
@@ -11,8 +12,12 @@ public class Producer extends Thread{
     public void run(){
         while(true)
         {
+          /*
+          Requests are made with random ID between 1 and 100
+          length of the reuqest is between 1 and maxlength
+          */
           reqLength = (int)Math.floor((Math.random()*(double)l)+1);
-          ID = (int)Math.floor(Math.random()*100.00);
+          ID = (int)Math.floor((Math.random()*100.00)+1);
           int[] request = new int[]{ID, reqLength};
               try{
                 Main.locker.enqueue(request);
