ff4297a58ce400ea0e53df4c6563b5fed1d9183d
WolffM <mjwolff@umass.edu>
2016-04-26 18:21:42 -0400

    Update Superblock.java

--- Lab3Part2/Superblock.java
+++ Lab3Part2/Superblock.java
@@ -1,10 +1,12 @@
 import java.io.Serializable;
 
+//Singleton object which contains all the inodes and freeblocklist
 public class Superblock implements Serializable{
 	
 	inode [] inodes = new inode[16];
 	byte [] freeBlockList = new byte[128];
 	
+	//Defaults all inodes and their blockpointers on creation
 	public Superblock(){
 		freeBlockList[0] = 1;
 		initializeInodes();
@@ -24,50 +26,6 @@ public class Superblock implements Serializable{
 			inodes[i] = new inode(8);
 			inodes[i].setBlockPointers((8*i)+1);
 		}
-	}
-	public void readInodes(byte[] data){
-		byte[] temp = new byte[8];
-		byte[] temp2 = new byte[4];
-		byte[] temp3 = new byte[32];
-		byte[] temp4 = new byte[4];
-		for(int i = 0; i < inodes.length; i++){
-			for(int ix = 0; ix < 8; ix++){
-				temp[ix] = data[48*i+ix+1];
-			}
-			inodes[i].name = temp.toString().toCharArray();
-			
-			for(int iy = 0; iy < 4; iy++){
-				temp2[iy] = data[48*i+iy+9];
-			}
-			inodes[i].size = byteArrayToInt(temp2)[0];
-			for(int iz = 0; iz < 32; iz++){
-				temp3[iz] = data[48*i+iz+13];
-			}
-			inodes[i].blockPointers = byteArrayToInt(temp3);
-			
-			for(int iw = 0; iw < 4; iw++)
-			{
-				temp4[iw] = data[48*i+iw+45];
-			}
-			inodes[i].used = byteArrayToInt(temp4)[0];
-		}
-	}
-	public static int[] byteArrayToInt(byte [] b)
-	{
-		int[] answer = new int[b.length/4];
-		int sum = 0;
-		
-		for(int i = 0; i < answer.length; i++)
-		{
-			sum = 0;
-			for(int j = 0; j < b.length; j++)
-			{
-				int lshift = (4-1-j)*8;
-				sum+= (b[j]& 0x000000FF) << lshift;
-			}
-			answer[i] = sum;
-		}
-		return answer;
 	}
 	public void changeBlock(int position, byte change)
 	{
